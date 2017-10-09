acdcd92fb930804156ba4974d3fc1832dba77473
Rodolfo Emmanuel Cab Martinez <14470207@itcampeche.edu.mx>
2017-10-09 12:10:14 -0600

    ContadorApp: Se agrego el manejo de eventos a la interfaz grafica

--- app/src/main/java/com/rodolfocab/android/contadorapp/MainActivity.java
+++ app/src/main/java/com/rodolfocab/android/contadorapp/MainActivity.java
@@ -1,13 +1,36 @@
 package com.rodolfocab.android.contadorapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 
 public class MainActivity extends AppCompatActivity {
+    private Button mBotonContar, mBotonReiniciar;
+    private EditText mTextoContar;
+    int mcontar = 0;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate (Bundle savedInstance) {
+        super.onCreate(savedInstance);
         setContentView(R.layout.activity_main);
+
+        mBotonContar = (Button)findViewById(R.id.BotonContar);
+        mBotonReiniciar = (Button)findViewById(R.id.BotonReiniciar);
+        mTextoContar = (EditText) findViewById(R.id.BotonIncrementar);
+
+        mBotonContar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mcontar++;
+                mTextoContar.setText(mcontar+"");
+
+            }
+        });
+
+
+
+
     }
-}
